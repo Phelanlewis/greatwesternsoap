50a67ace7c8205a236b37de60d46898fcaa6588c
Phelan Lewis <phelanlewis@gmail.com>
2017-04-17 11:31:02 -0700

    still more files that had merge conflicts... sigh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-<<<<<<< HEAD
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
@@ -24,18 +24,17 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-=======
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
->>>>>>> e6572a931f7cd61a3be0f62283f389c190d4e9c9
+# ======= solidus only method =======
+#   # Disable serving static files from the `/public` folder by default since
+#   # Apache or NGINX already handles this.
+#   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-<<<<<<< HEAD
   config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
@@ -44,27 +43,26 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
-=======
-  config.assets.compile = false
+# ======= solidus only method =======
+  # config.assets.compile = false
+  #
+  # # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  #
+  # # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # # config.action_controller.asset_host = 'http://assets.example.com'
 
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-
->>>>>>> e6572a931f7cd61a3be0f62283f389c190d4e9c9
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-<<<<<<< HEAD
-=======
+  # ======= solidus only method =======
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+# =======  =======
 
->>>>>>> e6572a931f7cd61a3be0f62283f389c190d4e9c9
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
