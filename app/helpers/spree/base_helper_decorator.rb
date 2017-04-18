0b47572f0389b7e53edbdd62c92a724a33a841a0
Phelan Lewis <phelanlewis@gmail.com>
2017-04-18 11:24:53 -0700

    Added taxon_breadcrumb to basehelper

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -17,4 +17,30 @@ module Spree::BaseHelper
     end
   end
 
+  def taxon_breadcrumbs(taxon, separator = '&nbsp;&raquo;&nbsp;', breadcrumb_class = 'inline')
+    return '' if current_page?('/') || taxon.nil?
+
+    crumbs = [[Spree.t(:home), spree.root_path]]
+
+    if taxon
+      crumbs << [Spree.t(:products), products_path]
+      crumbs += taxon.ancestors.collect { |a| [a.name, spree.nested_taxons_path(a.permalink)] } unless taxon.ancestors.empty?
+      crumbs << [taxon.name, spree.nested_taxons_path(taxon.permalink)]
+    else
+      crumbs << [Spree.t(:products), products_path]
+    end
+
+    separator = raw(separator)
+
+    items = crumbs.each_with_index.collect do |crumb, i|
+      content_tag(:li, itemprop: 'itemListElement', itemscope: '', itemtype: 'https://schema.org/ListItem') do
+        link_to(crumb.last, itemprop: 'item') do
+          content_tag(:span, crumb.first, itemprop: 'name') + tag('meta', { itemprop: 'position', content: (i+1).to_s }, false, false)
+        end + (crumb == crumbs.last ? '' : separator)
+      end
+    end
+
+    content_tag(:nav, content_tag(:ol, raw(items.map(&:mb_chars).join), class: breadcrumb_class, itemscope: '', itemtype: 'https://schema.org/BreadcrumbList'), id: 'breadcrumbs', class: 'sixteen columns')
+  end
+  
 end
