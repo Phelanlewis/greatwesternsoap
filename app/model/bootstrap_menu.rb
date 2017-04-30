50557191f4a85509e1fdd50c248a5f75c1faeca7
Phelan Lewis <phelanlewis@gmail.com>
2017-04-30 16:03:02 -0700

    navbar is not completely fixed... sigh

--- app/model/bootstrap_menu.rb
+++ app/model/bootstrap_menu.rb
@@ -1,7 +1,3 @@
 class BootstrapMenu < Refinery::Pages::MenuPresenter
-  def render_menu(items)
-    content_tag(:section, :id => "nav", :class => 'navbar') do
-      render_menu_items(items)
-    end
-  end
+  
 end
